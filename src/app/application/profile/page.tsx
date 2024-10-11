ec6ef7913fb462d8d7ede21b3041be19ff88dba9
Paul <mierlo.paul@hotmail.nl>
2024-10-11 07:38:34 +0200

    added suspense to useSearchparams

--- src/app/application/profile/page.tsx
+++ src/app/application/profile/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import { Linechart } from "@/components/ui/linechart";
 import ProfileIcon from "@/components/ui/icons/profileicon";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
@@ -43,52 +43,54 @@ export default function Profile() {
   }, [profileAddress]);
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <Button onClick={() => router.back()} variant="ghost" className="mb-6">
-        <ArrowLeft className="mr-2 h-4 w-4" /> Back
-      </Button>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="container mx-auto px-4 py-8">
+        <Button onClick={() => router.back()} variant="ghost" className="mb-6">
+          <ArrowLeft className="mr-2 h-4 w-4" /> Back
+        </Button>
 
-      <div className="flex flex-col gap-6">
-        <Card className="col-span-1 md:col-span-2">
-          <CardHeader>
-            <CardTitle className="text-2xl font-bold">Profile Information</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-col justify-center items-center">
-            <div className="flex flex-col items-center mb-6 w-24 h-24">
-              <ProfileIcon />
-              <h2 className="text-xl font-semibold">
-                {loading ? "Loading..." : username || "Username not found"} {/* Display loading state or username */}
-              </h2>
-            </div>
-            <div className="space-y-4 w-1/2">
-              <div>
-                <Label className="font-semibold">Account Address</Label>
-                <div className="flex items-center ">
-                  <Wallet className="mr-2 h-4 w-4 text-gray-500" />
-                  <Input value={account?.address} disabled={true} className="font-mono text-sm" />
+        <div className="flex flex-col gap-6">
+          <Card className="col-span-1 md:col-span-2">
+            <CardHeader>
+              <CardTitle className="text-2xl font-bold">Profile Information</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col justify-center items-center">
+              <div className="flex flex-col items-center mb-6 w-24 h-24">
+                <ProfileIcon />
+                <h2 className="text-xl font-semibold">
+                  {loading ? "Loading..." : username || "Username not found"} {/* Display loading state or username */}
+                </h2>
+              </div>
+              <div className="space-y-4 w-1/2">
+                <div>
+                  <Label className="font-semibold">Account Address</Label>
+                  <div className="flex items-center ">
+                    <Wallet className="mr-2 h-4 w-4 text-gray-500" />
+                    <Input value={account?.address} disabled={true} className="font-mono text-sm" />
+                  </div>
+                </div>
+
+                <div>
+                  <Label className="font-semibold">Name</Label>
+                  <div className="flex items-center">
+                    <User className="mr-2 h-4 w-4 text-gray-500" />
+                    <Input value={username} disabled={true} />
+                  </div>
                 </div>
               </div>
+            </CardContent>
+          </Card>
 
-              <div>
-                <Label className="font-semibold">Name</Label>
-                <div className="flex items-center">
-                  <User className="mr-2 h-4 w-4 text-gray-500" />
-                  <Input value={username} disabled={true} />
-                </div>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-
-        <Card className="col-span-1 flex flex-col justify-center items-center">
-          <CardHeader>
-            <CardTitle className="text-2xl font-bold">Activity Overview</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-col justify-center items-center h-fit w-3/4">
-            <Linechart />
-          </CardContent>
-        </Card>
+          <Card className="col-span-1 flex flex-col justify-center items-center">
+            <CardHeader>
+              <CardTitle className="text-2xl font-bold">Activity Overview</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col justify-center items-center h-fit w-3/4">
+              <Linechart />
+            </CardContent>
+          </Card>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
