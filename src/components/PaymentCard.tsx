cec34f596043d6902bd75b54cec1832712506cf8
0xmihirsahu <0xmihirsahu@gmail.com>
2024-10-12 02:24:49 +0530

    time rearrange

--- src/components/PaymentCard.tsx
+++ src/components/PaymentCard.tsx
@@ -4,7 +4,7 @@ import { Badge } from "./ui/badge";
 import { Payment } from '@/view-functions/getSentPayment';
 import { convertOctaToApt } from '@/core/utils';
 import { MoveAddressType } from '@aptos-labs/ts-sdk';
-import { CircleCheckBig } from 'lucide-react';
+import { CircleCheckBig, HandCoins } from 'lucide-react';
 
 interface PaymentCardProps {
   payment: Payment
@@ -17,10 +17,10 @@ const PaymentCard: React.FC<PaymentCardProps> = ({ payment, account })  => {
     <Card className="w-full max-w-sm  bg-zinc-900 text-white border-gray-700">
       <CardHeader>
         <CardTitle className="text-lg flex flex-row gap-2 font-semibold">{payment.sender == account ? "Payment Sent" : "Payment Received"}
-          <CircleCheckBig color='green' strokeWidth={3}/>
+          {payment.sender == account ? <CircleCheckBig color='green' strokeWidth={2} /> : <HandCoins color='green' strokeWidth={2} />}
         </CardTitle>
         <CardDescription className="text-gray-400">
-          {payment.timestamp}
+          From: {payment.sender.slice(0, 6)}...{payment.sender.slice(-4)}
         </CardDescription>
       </CardHeader>
       <CardContent>
@@ -34,8 +34,8 @@ const PaymentCard: React.FC<PaymentCardProps> = ({ payment, account })  => {
           <p><span className="font-medium">Note:</span> {payment.note}</p>
         </div>
       </CardContent>
-      <CardFooter className="text-xs text-gray-400">
-        From: {payment.sender.slice(0, 6)}...{payment.sender.slice(-4)}
+      <CardFooter className="text-xs font-medium text-gray-400">
+        {payment.timestamp}
       </CardFooter>
     </Card>
   );
--- src/view-functions/getSentPayment.ts
+++ src/view-functions/getSentPayment.ts
@@ -48,7 +48,6 @@ export const getSentPayment = async (
         const formattedTime = date.toLocaleTimeString("en-US", {
           hour: "2-digit",
           minute: "2-digit",
-          second: "2-digit",
         });
         return `${formattedTime} ${formattedDate}`;
       };
